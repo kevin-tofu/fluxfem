204d3988eda10394566980b56488931019c93c08
Kevin-Tofu <koheitech001@gmail.com>
2025-12-25 13:39:45 +0900

    add version info

--- src/fluxfem/__init__.py
+++ src/fluxfem/__init__.py
@@ -306,3 +306,38 @@ __all__ = [
     "max_shear_stress",
     "von_mises_stress",
 ]
+
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    # old python
+    from importlib_metadata import version, PackageNotFoundError
+
+
+def read_version_from_pyproject():
+    import pathlib
+    import re
+
+    root = pathlib.Path(__file__).resolve().parent.parent
+    # while root != root.parent and depth < max_depth:
+    pyproject_path = root / "pyproject.toml"
+    if pyproject_path.exists():
+        content = pyproject_path.read_text()
+        match = re.search(r'version\s*=\s*"(.*?)"', content)
+        if match:
+            return match.group(1)
+    # root = root.parent
+    #     depth += 1
+    return "0.0.0"
+
+
+def get_version(package_name):
+    try:
+        # print("version-", version(package_name))
+        return version(package_name)
+    except PackageNotFoundError:
+        return read_version_from_pyproject()
+
+
+__version__ = get_version("fluxfem")
