df82afc37ee8d95b672151b51485dab4bd51d508
Kevin-Tofu <koheitech001@gmail.com>
2026-04-07 03:27:19 +0900

    upgradev version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = "FluxFEM"
 author = "Kohei Watanabe"
 copyright = f"{datetime.now().year}, {author}"
-release = "0.3.4"
+release = "0.3.5"
 
 extensions = [
     "sphinx.ext.autodoc",
